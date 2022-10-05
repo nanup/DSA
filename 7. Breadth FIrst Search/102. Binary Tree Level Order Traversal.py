fb90d1229dc9eaac5ba7238619f90ccfae4e631c
nanup <nanup.personal+github@gmail.com>
2022-10-05 23:08:01 +0530

    solves using breadth first search

---
+++ 7. Breadth FIrst Search/102. Binary Tree Level Order Traversal.py
@@ -0,0 +1,53 @@
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+
+def traverse(root):
+    result = []
+
+    if not root:
+        return result
+
+    queue = deque()
+
+    queue.append(root)
+
+    while queue:
+        levelSize = len(queue)
+
+        currentNodes = []
+
+        for _ in range(levelSize):
+            node = queue.popleft()
+
+            currentNodes.append(node.val)
+
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+
+        result.append(currentNodes)
+
+    return result
+
+
+def main():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    print("Level order traversal: " + str(traverse(root)))
+
+
+main()
+
+# time complexity: O(N)
+# space complexity: O(N)
