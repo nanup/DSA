4fc03557678a78cae75319d2e16884f9f28dad70
nanup <nanup.personal+github@gmail.com>
2022-10-10 14:09:43 +0530

    solves using bfs

---
+++ 10. Subsets Revisit I/22. Generate Parentheses.py
@@ -0,0 +1,42 @@
+# For a given number ‘N’, write a function to generate
+# all combination of ‘N’ pairs of balanced parentheses.
+from collections import deque
+
+
+class ParenthesesString():
+	def __init__(self, str, openCount, closeCount):
+		self.str = str
+		self.openCount = openCount
+		self.closeCount = closeCount
+
+
+def generate_valid_parentheses(num):
+	result = []
+	queue = deque()
+	queue.append(ParenthesesString("", 0, 0))
+
+	while queue:
+		ps = queue.popleft()
+		if ps.openCount == num and ps.closeCount == num:
+			result.append(ps.str)
+
+		else:
+			if ps.openCount < num:
+				queue.append(ParenthesesString(ps.str + "(", ps.openCount + 1, ps.closeCount))
+			if ps.openCount > ps.closeCount:
+				queue.append(ParenthesesString(ps.str + ")", ps.openCount, ps.closeCount + 1))
+
+	return result
+
+
+def main():
+	print("All combinations of balanced parentheses are: " +
+		  str(generate_valid_parentheses(2)))
+	print("All combinations of balanced parentheses are: " +
+		  str(generate_valid_parentheses(3)))
+
+
+main()
+
+# time complexity: O(N * 2 ^ N)
+# space complexity: O(N * 2 ^ N)
--- 10. Subsets Revisit I/784. Letter Case Permutation.py
+++ 10. Subsets Revisit I/784. Letter Case Permutation.py
@@ -2,26 +2,50 @@
 
 def find_letter_case_string_permutations(str):
 	permutations = []
-	permutations.append(str)
-	
-	for i in range(len(str)):
-		for j in range(len(permutations)):
-			permutation = list(permutations[j])
-			if permutation[i].isalpha():
-				permutation[i] = permutation[i].swapcase()
-				permutations.append("".join(permutation))
+
+	# permutations.append(str)
+
+	# for i in range(len(str)):
+	# 	for j in range(len(permutations)):
+	# 		permutation = list(permutations[j])
+	# 		if permutation[i].isalpha():
+	# 			permutation[i] = permutation[i].swapcase()
+	# 			permutations.append("".join(permutation))
+
+	# return permutations
+
+	permutation = []
+
+	def permute(i):
+		if i == len(str):
+			permutations.append("".join(permutation.copy()))
+			return
+
+		char = str[i]
+
+		permutation.append(char)
+		permute(i + 1)
+
+		if char.isalpha():
+			for i in range(len(str) - i):
+				permutation.pop()
+			permutation.append(char.swapcase())
+			permute(i + 1)
+			
+	permute(0)
 
 	return permutations
-
+		
 
 def main():
 	print("String permutations are: " +
-		  str(find_letter_case_string_permutations("ad52")))
-	print("String permutations are: " +
-		  str(find_letter_case_string_permutations("ab7c")))
+		  str(find_letter_case_string_permutations("ab")))
+	# print("String permutations are: " +
+	# 	  str(find_letter_case_string_permutations("ab7c")))
 
 
 main()
 
 # time complexity: O(N * 2 ^ N)
 # space complexity: O(N * 2 ^ N)
+
