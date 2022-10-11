8dde0317e9942ff9cc9bc55b1a9909d2a1be1674
nanup <nanup.personal+github@gmail.com>
2022-10-11 18:55:13 +0530

    solves using xor and hashmap

---
+++ 12. Bitwise XOR/136. Single Number.py
@@ -0,0 +1,34 @@
+# In a non-empty array of integers, every number appears twice
+# except for one, find that single number.
+
+def find_single_number(arr):
+	# x1 = arr[0]
+
+	# for i in range(1, len(arr)):
+	# 	x1 = x1 ^ arr[i]
+
+	# return x1
+
+	hashMap = dict()
+
+	for num in arr:
+		if num not in hashMap:
+			hashMap[num] = 0
+		else:
+			del hashMap[num]
+
+	return list(hashMap.keys())[0]
+
+
+def main():
+	arr = [1, 4, 2, 1, 3, 2, 3]
+	print(find_single_number(arr))
+
+
+main()
+
+# time complexity: O(N)
+# space complexity: o(1)
+
+# time complexity: O(N)
+# space complexity: O(N)
