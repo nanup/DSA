8ee39c86fb450ec97f5e0a298b3338093b25d325
nanup <nanup.personal+github@gmail.com>
2022-10-12 19:03:08 +0530

    solves using binary search and heap

--- 13. Top 'K' Elements/658. Find K Closest Elements.py
+++ 13. Top 'K' Elements/658. Find K Closest Elements.py
@@ -48,7 +48,7 @@ def main():
 	print("'K' closest numbers to 'X' are: " +
 		  str(find_closest_elements([2, 4, 5, 6, 9], 3, 6)))
 	print("'K' closest numbers to 'X' are: " +
-		  str(find_closest_elements([2, 4, 5, 6, 9], 3, 10)))
+		  str(find_closest_elements([0,0,1,2,3,3,4,7,7,8], 3, 5)))
 
 
 main()
